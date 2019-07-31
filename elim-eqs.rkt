38dd867eb6070a0cf5741dc865ac25237ece5e1b
Shaobo He <polarishehn@gmail.com>
2019-07-30 22:49:32 -0600

    added elim-eqs.rkt

---
+++ elim-eqs.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+(require "parsing/parse.rkt")
+
+(define (format-id str)
+  (string-replace str "|" "❚"))
+
+(define (reformat-id str)
+  (string-replace str "❚" "|"))
+
+;; use Z3's solve-eqs tactic to remove equalities
+(define (eliminate-eqs file)
+  (define temp-file (make-temporary-file "rkttmp~a" file))
+  (define raw-content (file->string file #:mode 'text))
+  (define raw-expr (string->sexp raw-content))
+  (define decls (filter
+                 (λ (e) (match e
+                          [`(declare-const ,id ,type) #t]
+                           [`(declare-fun ,id () ,type) #t]
+                           [_ #f]))
+                 raw-expr))
+  (displayln decls)
+  (call-with-output-file
+      temp-file
+    (λ (output-port)
+      (display
+        raw-content
+        "check-sat"
+        "apply solve-eqs"
+       output-port))
+    #:mode 'text
+    #:exists 'replace)
+  (define z3-output
+    (string->sexp
+     (with-output-to-string
+       (thunk (system (~v "z3" (path->string temp-file)))))))
+  (displayln z3-output))
+
+(eliminate-eqs (vector-ref (current-command-line-arguments) 0))
